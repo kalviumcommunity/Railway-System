b9957b43399e8e28c4456387f7bbc64ecb55f644
harishb2006 <imharishba@gmail.com>
2026-01-23 12:48:11 +0530

    done this initialisation

---
+++ my-next/app/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <div className="">
+
+      <h1 className="text-3xl font-bold "> 
+        Welcome to Railway System
+      </h1> 
+         
+
+      
+    </div>
+  );
+}
